3e1c7ddf0da8b02df4d9e294e50eb79a2ef4a972
MariaIsaBernardo <maria.i.bernardo@edu.senai.br>
2026-05-29 13:58:00 -0300

    sces.py

--- sces.py
+++ sces.py
@@ -1,18 +1,5 @@
 
 estoque = []
-
-def adicionar_produto():
-    print("\n--- Função Adicionar Produto ---")
-
-def listar_produtos():
-    print("\n--- Função Listar Produtos  ---")
-
-def buscar_produto_por_id():
-    print("\n--- Função Buscar por ID ---")
-
-def atualizar_estoque():
-    print("\n--- Função Atualizar Estoque ---")
-
 def menu():
     while True:
         print("\n----- Sistema De Controle De Estoque Simplificado (SCES) -----")
@@ -25,18 +12,3 @@ def menu():
         opcao = input("Opção: ")
         
         if opcao == "1":
-            adicionar_produto()
-        elif opcao == "2":
-            listar_produtos()
-        elif opcao == "3":
-            buscar_produto_por_id()
-        elif opcao == "4":
-            atualizar_estoque()
-        elif opcao == "5":
-            print("Saindo...")
-            break
-        else:
-            print("Opção inválida!")
-
-
-menu()
