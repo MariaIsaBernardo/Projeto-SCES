8e5b00435abfd2588d90bd4ecc7ea32fe232735b
MariaIsaBernardo <maria.i.bernardo@edu.senai.br>
2026-06-18 08:54:40 -0300

    Adicionei os códigos das outras opções e mudei algumas coisas mais estéticas

--- sces.py
+++ sces.py
@@ -1,26 +1,14 @@
-
 estoque = []
-
-def adicionar_produto():
-    print("\n--- Função Adicionar Produto ---")
-
-def listar_produtos():
-    print("\n--- Função Listar Produtos  ---")
-
-def buscar_produto_por_id():
-    print("\n--- Função Buscar por ID ---")
-
-def atualizar_estoque():
-    print("\n--- Função Atualizar Estoque ---")
 
 def menu():
     while True:
-        print("\n--- MENU SCES ---")
-        print("1. Adicionar")
-        print("2. Listar")
-        print("3. Buscar")
-        print("4. Atualizar")
-        print("5. Sair")
+        print("\n--- Menu SCES ---")
+        print("\nBem vindo ao menu interativo da SCES! Por favor selecione uma opção:\n")
+        print("1 - Adicionar")
+        print("2 - Listar")
+        print("3 - Buscar")
+        print("4 - Atualizar")
+        print("5 - Sair")
         
         opcao = input("Opção: ")
         
@@ -29,7 +17,7 @@ def menu():
         elif opcao == "2":
             listar_produtos()
         elif opcao == "3":
-            buscar_produto_por_id()
+            buscarproduto_id()
         elif opcao == "4":
             atualizar_estoque()
         elif opcao == "5":
